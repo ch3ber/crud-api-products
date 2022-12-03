3cab689ea6a616f0f2db5c7894cd279a7c05d4e7
Eber Alejo <eber4202@hotmail.com>
2022-12-02 22:19:34 -0600

    [ADD] frontend

--- index.js
+++ index.js
@@ -22,8 +22,22 @@ const options = {
 app.use(cors(options))
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express')
+  res.send(`Hola mi primer server en express.
+  <br>
+  Rutas disponibles:
+  <ul>
+    <li>/api/v1</li>
+    <li>/products</li>
+    <li>/categories</li>
+    <li>/users</li>
+  </ul>
+  `)
 })
+
+// app.use('/api/v1', router)
+// router.use('/products', productsRouter)
+// router.use('/categories', categoriesRouter)
+// router.use('/users', usersRouter)
 
 routerApi(app)
 
